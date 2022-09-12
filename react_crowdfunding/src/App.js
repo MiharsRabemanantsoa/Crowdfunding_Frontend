14537a4e7257b7a3bdc1829a25faeb969f5e332e
Mendrika <mendrikatyswork@gmail.com>
2022-09-12 16:07:59 +0300

    appel tous les projets populaire avec projets populaire  services

--- react_crowdfunding/src/App.js
+++ react_crowdfunding/src/App.js
@@ -8,71 +8,72 @@ import { BrowserRouter as Router,Route,Routes,Link } from 'react-router-dom';
 import Accueil from './Components/Accueil';
 import LesProjets from './Components/LesProjets';
 
+
 function App() {
   return (
     <Router>
-    <div className="Container">
-      
-      {/* <Navbar></Navbar> */}
-      
-      {/* <div className="loader">
-                <div className="d-table">
-                    <div className="d-table-cell">
-                        <div className="pre-box-one">
-                            <div className="pre-box-two"></div>
+        <div className="Container">
+        
+        {/* <Navbar></Navbar> */}
+        
+        {/* <div className="loader">
+                    <div className="d-table">
+                        <div className="d-table-cell">
+                            <div className="pre-box-one">
+                                <div className="pre-box-two"></div>
+                            </div>
                         </div>
+                    </div>
+            </div> */}
+
+            <div className="navbar-area sticky-top">
+
+            {/* <div className="mobile-nav">
+                <Link to="/home" className="logo">
+                    <img src={logo_clair} alt="Logo"/>
+                </Link>
+            </div> */}
+        
+                <div className="main-nav">
+                    <div className="container">
+                        <nav className="navbar navbar-expand-md navbar-light">
+                            <Link className="navbar-brand" to="/home">
+                                <img src={logo_final} className="logo-one" alt="Logo"/>
+                            </Link>
+                            <div className="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <Link to="/home" className="nav-link active">Accueil</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/projets" className="nav-link">Tous les projets</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/Apropos" className="nav-link">A propos</Link>
+                                    </li>
+                                    <li className="nav-item">
+                                        <Link to="/contactUs" className="nav-link">Contact</Link>
+                                    </li>
+                                </ul>
+                                <div className="side-nav">
+                                    <Link className="nav-link" to="/connexion">Se connecter</Link>
+                                </div>
+                            </div>
+                        </nav>
                     </div>
                 </div>
-        </div> */}
-
-        <div className="navbar-area sticky-top">
-
-        {/* <div className="mobile-nav">
-            <Link to="/home" className="logo">
-                <img src={logo_clair} alt="Logo"/>
-            </Link>
-        </div> */}
-    
-        <div className="main-nav">
-            <div className="container">
-                <nav className="navbar navbar-expand-md navbar-light">
-                    <Link className="navbar-brand" to="/home">
-                        <img src={logo_final} className="logo-one" alt="Logo"/>
-                    </Link>
-                    <div className="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
-                        <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link to="/home" className="nav-link active">Accueil</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/projets" className="nav-link">Tous les projets</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/Apropos" className="nav-link">A propos</Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to="/contactUs" className="nav-link">Contact</Link>
-                            </li>
-                        </ul>
-                        <div className="side-nav">
-                            <Link className="nav-link" to="/connexion">Se connecter</Link>
-                        </div>
-                    </div>
-                </nav>
             </div>
-        </div>
-    </div>
-    <Routes>
-     <Route path='/home' element={<Accueil/>} />
-     <Route path='/allproject' element={<LesProjets/>}/>     
-     </Routes>
-     
+        <Routes>
+        <Route path='/home' element={<Accueil/>} />   
+        <Route path='/allproject' element={<LesProjets/>}/>     
+        </Routes>
+        
 
-      <div className="go-top">
-        <i className="bi bi-arrow-up-short"></i>
-        <i className="bi bi-arrow-up-short"></i>
-      </div>
-    </div>
+        <div className="go-top">
+            <i className="bi bi-arrow-up-short"></i>
+            <i className="bi bi-arrow-up-short"></i>
+        </div>
+        </div>
     </Router>
   );
 
--- react_crowdfunding/src/Components/ProjetPopulaire.js
+++ react_crowdfunding/src/Components/ProjetPopulaire.js
@@ -1,116 +1,68 @@
-import React, { Component } from 'react'
+import React,{Component} from 'react';
 import Card from'./Card'
+import ProjetPopulaireService from '../Services/ProjetPopulaireService';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
+const responsive = {
+    superLargeDesktop: {
+      // the naming can be any, depends on you.
+      breakpoint: { max: 4000, min: 3000 },
+      items: 5
+    },
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1
+    }
+  };
 
 export default class ProjetPopulaire extends Component {
-constructor(props){
-    super(props)
-    this.state={
-        projetPoulaire :[]
+    constructor(props){
+        super(props)
+        this.state={
+            projets :[]
+        }
     }
-}
+
+    componentDidMount(){
+        ProjetPopulaireService.getProjet().then((result)=>{
+            this.setState({projets : result.data})
+        }
+        )
+    }
   
 
   render() {
     return (
       <section className="blog-area pt-100 pb-70">
-      <div className="container">
-          <div className="section-title">
-                  <h2 style={{fontWeight:700}}>Projets Populaires</h2>
-              <p style={{color:'#757575',fontFamily:"Open Sans"}}>Fructifiez vos épargnes dans des projets impactant et sûrs.</p>
-          </div>
-          <div className="row">
-              {/* <div className="col-sm-6 col-lg-4">
-                  <div className="blog-item">
-                      <div className="top">
-                          <a href="blog-details.html">
-                              <img alt="Blog"/>
-                          </a>
-                      </div>
-                      <div className="bottom">
-                          <ul>
-                              <li>
-                                  <i className="bi-calendar"></i>
-                                      <span>21 Jan, 2020</span>
-                              </li>
-                              <li>
-                                  <i className="bi-person"></i>
-                                      <span>By:</span>
-                                  <a href="index.html">Admin</a>
-                              </li>
-                          </ul>
-                              <h3>
-                                  <a href="blog-details.html">Donate for nutration less poor people</a>
-                              </h3>
-                              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus dolor laudantium distinction</p>
-                          <a className="blog-btn" href="blog-details.html">En savoir plus</a>
-                      </div>
-                  </div>
-              </div>
-                <div className="col-sm-6 col-lg-4">
-                    <div className="blog-item">
-                        <div className="top">
-                            <a href='/' className="blog-details.html">
-                                <img  alt="Blog"/>
-                            </a>
-                        </div>
-                        <div className="bottom">
-                            <ul>
-                                <li>
-                                    <i className="bi-calendar"></i>
-                                        <span>22 Jan, 2020</span>
-                                </li>
-                                <li>
-                                    <i className="bi-person-fill"></i>
-                                        <span>By:</span>
-                                    <a href="index.html">Admin</a>
-                                </li>
-                            </ul>
-                        <h3>
-                        <a href="blog-details.html">Charity meetup in Berline next year</a>
-                        </h3>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus dolor laudantium distinction</p>
-                        <a className="blog-btn" href="blog-details.html">En savoir plus</a>
-                        </div>
-                    </div>
-                </div>
-              <div className="col-sm-6 offset-sm-3 offset-lg-0 col-lg-4">
-                  <div className="blog-item">
-                      <div className="top">
-                          <a href="blog-details.html">
-                              <img alt="Blog"/>
-                          </a>
-                      </div>
-                      <div className="bottom">
-                          <ul>
-                              <li>
-                                  <i className="bi-calendar"></i>
-                                      <span>23 Jan, 2020</span>
-                              </li>
-                              <li>
-                                  <i className="bi-person-fill"></i>
-                                      <span>By:</span>
-                                  <a href="index.html">Admin</a>
-                              </li>
-                          </ul>
-                              <h3>
-                                  <a href="blog-details.html">Donate for the poor people to help them</a>
-                              </h3>
-                              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet cupiditate sit ducimus dolor laudantium distinction</p>
-                          <a className="blog-btn" href="blog-details.html">En savoir plus</a>
-                      </div>
-                  </div>
-              </div> */}
-              { this.state.projetPopulaire.map((projetPolaire,index)=>
-                            
-                            <Card projet={projet} key={projet.idProjet}></Card> 
-                             ) 
-                        }
-          </div>
-          <div className="section-title">
-              <a className="common-btn banner-btn" href="projets.html">Voir tous les projets</a>
-          </div>
-      </div>
-  </section>
+        <div className="container">
+            <div className="section-title">
+                    <h2 style={{fontWeight:700}}>Projets Populaires</h2>
+                <p style={{color:'#757575',fontFamily:"Open Sans"}}>Fructifiez vos épargnes dans des projets impactant et sûrs.</p>
+            </div>
+            <div className="row">
+                
+                <Carousel axis='horizontal'selectedItem='3'  responsive={responsive}>
+                            { this.state.projets.map((projet,index)=>
+                                
+                                <Card projet={projet} key={projet.idProjet}></Card> 
+                                ) 
+                            }
+                </Carousel>
+            </div>
+            <div className="section-title">
+                <a className="common-btn banner-btn" href="projets.html">Voir tous les projets</a>
+            </div>
+        </div>
+      </section>
     )
   }
 }
